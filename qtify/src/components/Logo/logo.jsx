24e562aafda372f54e63c5a21aee10da26fa13ef
Ameena Khanum <ameenakhanum@Ameenas-MacBook-Pro.local>
2024-05-03 17:30:55 +0530

    albums

--- qtify/src/components/Logo/logo.jsx
+++ qtify/src/components/Logo/logo.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import logo from "../../Assets/logo.png";
+import "./Logo.css"; // Import the CSS file for styling
+
 const Logo = () => {
-  return <img src={logo} width={67} alt="logo not available" />;
+  return (
+    <div className="logo-container">
+      <img src={logo} width={67} alt="Logo" className="logo-img" />
+    </div>
+  );
 };
 
 export default Logo;
